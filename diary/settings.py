ef126447e0d50ffb1415468fb36907d4bb6e2403
Alexander Eriksson <alexander.eriksson.dev@gmail.com>
2021-10-21 23:18:51 +0200

    Cleaned up settings

--- diary/settings.py
+++ diary/settings.py
@@ -17,13 +17,9 @@ import django_heroku
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
+# LOADS SECRET KEY FROM ENVIRONEMT VARIABLE IF ON SERVER!
 SECRET_KEY = os.getenv('SECRET_KEY', '2as@_shbjydxtqidqq45axa#bt8@7ynqt4w@mphn541g&km0')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['warm-scrubland-19058.herokuapp.com', '127.0.0.1', 'localhost']
@@ -75,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'diary.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -107,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
