eae9469cd945fba5185cedde2dc44f3bdae55896
Alexander Eriksson <alexander.eriksson.dev@gmail.com>
2021-10-21 21:54:37 +0200

    Updated settings.py

--- diary/settings.py
+++ diary/settings.py
@@ -31,7 +31,8 @@ STATICFILES_DIRS = (
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
