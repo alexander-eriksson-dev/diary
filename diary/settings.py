5e9e62b98f1c685dbc964f0c27dd8dd06fbfd3e1
Alexander Eriksson <alexander.eriksson.dev@gmail.com>
2021-10-21 22:56:53 +0200

    Make Heroku deployment use secret key from environment variables

--- diary/settings.py
+++ diary/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2as@_shbjydxtqidqq45axa#bt8@7ynqt4w@mphn541g&km0'
+SECRET_KEY = os.getenv('SECRET_KEY', '2as@_shbjydxtqidqq45axa#bt8@7ynqt4w@mphn541g&km0')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
